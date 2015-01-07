f32a6ab03da6df4a366c7e5a0489bb8503a9656f
Vahan Nanumyan <vahan@nanumyan.com>
2015-01-07 16:13:20 +0100

    More description in README

--- .emacs.d/sg_init/visuals.el
+++ .emacs.d/sg_init/visuals.el
@@ -16,16 +16,15 @@
 (setq ring-bell-function 'ignore)
 
 ;; start maximized
-(toggle-frame-maximized)
+;(toggle-frame-maximized)
 
 ;; start maximized - FOR EMACS OLDER THAN 24.4
-;(defun toggle-fullscreen ()
-;  (interactive)
-;  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32	 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-;  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32	 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-;  )
-;(toggle-fullscreen)
-;(global-set-key [f11] 'toggle-fullscreen)
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32	 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32	 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+  )
+(toggle-fullscreen)
 
 ;; opacity when Emacs not on focus 
 (set-frame-parameter (selected-frame) 'alpha '(100 90)) ;  '(<active> [<inactive>])
